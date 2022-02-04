684659db429cb5e2747ca9db880ecdddb902dcda
mialyssa <97639434+mialyssa@users.noreply.github.com>
2022-02-04 12:55:54 -0800

    fixed failed test

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTester {
     @Test
     public void multiplicationTester() {
-        assertEquals(10, SkillDemo.multiplication(5,3)); //10 != 15
+        assertEquals(10, SkillDemo.multiplication(5,2)); //10 != 15
     }
 }
